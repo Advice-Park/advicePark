582aacd5bdbe284a31c64a34e6ef95fffc63872b
dy <gkekdus97@naver.com>
2024-02-28 19:00:41 +0900

    feat:로그인버튼

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -20,13 +20,17 @@ const Login: React.FC = () => {
     //   });
   }, []);
 
+  const redirectUri = "http://advice-p-front.s3-website.ap-northeast-2.amazonaws.com/"
+  const loginUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_APP_GOOGLE_AUTH_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=email+profile`
+
   const handleLogin = () => {
-    window.location.href = "/api/oauth2/google";
+    window.location.href = loginUrl
   }
     return <button
     className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+    onAbort={handleLogin}
   >
-    로그인
+    소셜 로그인 가기
   </button>;
   
 };
