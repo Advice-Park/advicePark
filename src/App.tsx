c8967eae1c9f825308c2e8cc9964b6d66eb672ae
dy <gkekdus97@naver.com>
2024-02-28 18:11:07 +0900

    fix: 기본세팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+// import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import "./App.css";
 import LoginPage from './pages/LoginPage';
 import NotFound from './pages/NotFound';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
       <Routes>
-        {/* <Route path="/" exact={true} element={<Home />} /> */}
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
   );
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,27 +1,34 @@
 import React, { useEffect } from "react";
-import axios from "axios";
+// import instance from "../../services/instance";
 
 const Login: React.FC = () => {
-    useEffect(() => {
-      axios
-        .get("http://43.203.215.148:8080/api/oauth2/google")
-        .then((response) => {
-          console.log("응답 데이터:", response.data);
-          const url = response.data.url;
-          console.log("URL:", url);
+  useEffect(() => {
+    // instance
+    //   .get("/api/oauth2/google")
+    //   .then((response) => {
+    //     console.log("응답 데이터:", response.data);
+    //     const url = response.data.url;
+    //     console.log("URL:", url);
+    //     if (url) {
+    //       window.location.href = url;
+    //     } else {
+    //       console.error("서버로부터 받은 URL이 없습니다.");
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error("Google URL get 응답 오류:", error);
+    //   });
+  }, []);
 
-          if (url) {
-            window.location.href = url;
-          } else {
-            console.error("서버로부터 받은 URL이 없습니다.");
-          }
-        })
-        .catch((error) => {
-          console.error("Google URL get 응답 오류:", error);
-        });
-    }, []);
-
-  return <>로그인</>;
+  const handleLogin = () => {
+    window.location.href = "/api/oauth2/google";
+  }
+    return <button
+    className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+  >
+    로그인
+  </button>;
+  
 };
 
 export default Login;
