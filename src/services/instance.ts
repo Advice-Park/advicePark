0cadf1e146cbc63ec82e73ad816badc94485f5a0
dy <gkekdus97@naver.com>
2024-03-06 22:57:35 +0900

    feat: 리액트 쿠키 사용방식 변경

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
+import { getCookie } from "../components/cookie/reactCookie";
 
-const instance = axios.create({
+export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_URL,
+  timeout: 1000,
+  headers: { Authorization: `Bearer${getCookie("token")}` },
 });
 
-export default instance;
+// instance.interceptors.request.use(
+//   function (config) {
+//       config.headers.Authorization = `Bearer${cookies.token}`;
+//       return config;
+//   },
+//   function (error) {
+//       console.log("인터셉터 에러가 있습니다.");
+//       return Promise.reject(error);
+//   }
+// );
