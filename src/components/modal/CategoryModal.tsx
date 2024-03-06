3715e479ec60a9a3fb5d1f03f5e654c35883788a
dy <gkekdus97@naver.com>
2024-03-06 23:41:01 +0900

    feat: 글쓰기 post 초안

---
+++ src/components/modal/CategoryModal.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+
+interface Props {
+  open: boolean;
+  close: () => void;
+  parentFunction: (x: any) => void;
+}
+const CategoryModal: React.FC<Props> = ({ open, close, parentFunction }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  const [isActive, setIsActive] = useState<number>();
+
+  const closeModal = () => {
+    close();
+  };
+
+  useEffect(() => {
+    function handleCategorySetUp() {
+      if (isActive !== null) {
+        return closeModal();
+      }
+    }
+    handleCategorySetUp();
+
+    // function handleClickOutside(event: React.MouseEvent<Document>) {
+    //   if (
+    //     modalRef.current &&
+    //     !modalRef.current.contains(event.target as Node)
+    //   ) {
+    //     closeModal();
+    //   }
+    // }
+
+    // document.addEventListener("mousedown", handleClickOutside);
+
+    // return () => {
+    //   document.removeEventListener("mousedown", handleClickOutside);
+    // };
+  }, [modalRef, isActive]);
+
+  const [category, setCategory] = useState("카테고리");
+  const Categories = ["일상(잡답)", "연애", "운동", "음식", "기타"];
+
+  parentFunction(category);
+
+  const categoryHandler = (index: number) => {
+    setIsActive(index);
+    setCategory(Categories[index]);
+  };
+
+  return open ? (
+    <>
+      <div onClick={close} />
+      <div ref={modalRef}>
+        <div>
+          <div>
+            {Categories.map((item, idx) => (
+              <div
+                onClick={() => categoryHandler(idx)}
+                key={idx}
+                // isFirst={idx === 0}
+                // isClicked={isActive}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  ) : null;
+};
+
+export default CategoryModal;
--- src/pages/Post/PostPage.tsx
+++ src/pages/Post/PostPage.tsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PostFooter from "../../components/footer/PostFooter";
+import CategoryModal from "../../components/modal/CategoryModal";
+import { instance } from "../../services/instance";
 
 const PostPage: React.FC = () => {
-  return (
-    <div>글등록하는 페이지</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default PostPage
+  const WriteCallback = (x: any) => {
+    setCategory(x);
+  };
+
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const [category, setCategory] = useState("카테고리");
+  const [voting, setVoting] = useState(false);
+
+  // 이미지 업로드
+  const [imgs, setImgs] = useState<File[]>([]);
+
+  // 게시글 등록
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("contents", contents);
+    formData.append("category", category);
+    formData.append("votingEnabled", voting.toString());
+
+    for (let i = 0; i < imgs.length; i++) {
+      formData.append("imageFiles", imgs[i]);
+    }
+
+    if (title.length < 3 || title.length > 25) {
+      alert("제목은 3자 이상, 25자 이하여야 합니다!");
+      return;
+    }
+
+    if (contents.length < 10 || contents.length > 1000) {
+      alert("내용은 10자 이상, 1000자 이하여야 합니다!");
+      return;
+    }
+
+    if (category === "카테고리") {
+      alert("카테고리를 선택해 주세요.");
+      return;
+    }
+
+    // setLoading(true);
+
+    try {
+      await instance.post("/api/post", formData);
+      // setLoading(false);
+      alert("글 작성에 성공하였습니다.");
+      navigate("/posts");
+    } catch (e: any) {
+      // const errorMsg = e.response.data.message;
+      // alert(`${errorMsg}`);
+      console.log(e);
+    }
+  };
+
+  const handleCanc = () => {
+    navigate(-1);
+  };
+
+  const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false);
+
+  const categoryOpen = () => {
+    setIsCategoryModalOpen(true);
+  };
+
+  const categoryClose = () => {
+    setIsCategoryModalOpen(false);
+  };
+
+  return (
+    <form>
+      <div>
+        <p onClick={handleCanc}>취소</p>
+        <p onClick={submitHandler}>등록</p>
+      </div>
+      <div>
+        <div className="flex flex-col">
+          <p onClick={categoryOpen}>{category}</p>
+          <label>
+            <input
+              type="checkbox"
+              checked={voting}
+              onChange={() => setVoting(!voting)}
+            />
+            <span>찬반</span>
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            autoFocus
+            placeholder="제목"
+          />
+          <input
+            type="text"
+            value={contents}
+            onChange={(e) => {
+              setContents(e.target.value);
+            }}
+            placeholder="훈수 받고 싶은 내용을 입력하세요."
+          />
+        </div>
+        <PostFooter setImgs={setImgs} />
+        {/* {isCategoryModalOpen && ( */}
+        <CategoryModal
+          open={isCategoryModalOpen}
+          close={categoryClose}
+          parentFunction={WriteCallback}
+        />
+        {/* )} */}
+      </div>
+    </form>
+  );
+};
+
+export default PostPage;
