0be42e62fa7e4a1a6f0d698b1122a0097327656c
dy <gkekdus97@naver.com>
2024-03-07 22:53:23 +0900

    feat: 글목록 초안

--- src/pages/Posts/PostsPage.tsx
+++ src/pages/Posts/PostsPage.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Posts, getPosts } from "../../services/api/posts";
 
 const PostsPage: React.FC = () => {
-  return (
-    <div>글 목록이 쥬르륵</div>
-  )
-}
+  const [posts, setPosts] = useState<Posts[]>([]);
 
-export default PostsPage
+  useEffect(() => {
+    getPosts().then((res) => {
+      res ? setPosts(res) : console.log("글이 없습니다");
+    });
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <ul>
+          <li>{post.title}</li>
+          <li>{post.contents}</li>
+          <li>{post.category}</li>
+          <li>{post.imageUrls}</li>
+          <li>{post.votingEnabled}</li>
+          <li>{post.viewCount}</li>
+          <li>{post.commentCount}</li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default PostsPage;
