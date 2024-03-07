81c04d943a6f4b242a0fbcb8309c5e7c1899a19f
dy <gkekdus97@naver.com>
2024-03-07 21:39:46 +0900

    fix: 임시파일로부터의 에러 제거

--- src/services/UserAPI.ts
+++ src/services/UserAPI.ts
@@ -1,10 +1,10 @@
-import instance from "./instance";
+// import { instance } from "./instance"
 
-export const get = async () => {
-    try {
-        const res = await instance.get('/api/')
-        return res
-    } catch (err) {
-        console.log('에러 :', err)
-    }
-}
+// export const get = async () => {
+//     try {
+//         const res = await instance.get('/api/')
+//         return res
+//     } catch (err) {
+//         console.log('에러 :', err)
+//     }
+// }
