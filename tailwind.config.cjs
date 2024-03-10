82b3aba1a3ef10a54b5c03bdc07ee5de9164e7be
dy <gkekdus97@naver.com>
2024-03-10 15:19:39 +0900

    fix: max-width 수정 중

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {screens: {
-      sm: { min: "390px", max: "819px" },
-      md: { min: "820px" },
-    },},
+    extend: {
+      screens: {
+        sm: { min: "390px", max: "819px" },
+        md: { min: "820px" },
+      },
+    },
   },
   plugins: [],
-}
-
+};
