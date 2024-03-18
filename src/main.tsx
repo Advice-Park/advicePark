0c91928b53b32990d16dd28930ad79e624f95a87
dy <gkekdus97@naver.com>
2024-03-18 17:08:31 +0900

    fix: recoil 상태관리 최상단 부모로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import { RecoilRoot } from "recoil";
 import NotFound from "./pages/NotFound.tsx";
@@ -33,12 +30,12 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // 리액트 쿠키
-  <CookiesProvider>
-      {/* Recoil 상태관리 */}
-      <RecoilRoot>
-        {/* 리액트 router */}
-        <RouterProvider router={router} />
-      </RecoilRoot>
-  </CookiesProvider>
+  //  Recoil 상태관리
+  <RecoilRoot>
+    {/* 리액트 쿠키  */}
+    <CookiesProvider>
+      {/* 리액트 router */}
+      <RouterProvider router={router} />
+    </CookiesProvider>
+  </RecoilRoot>
 );
