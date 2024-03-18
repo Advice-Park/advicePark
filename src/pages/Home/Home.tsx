fd50e2850c5969591e6ce039ba83860062aa8c6a
dy <gkekdus97@naver.com>
2024-03-18 22:00:24 +0900

    feat: 로그인 홈 안내 문구 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import HotPost from "../../components/Home/HotPost";
+import { useRecoilValue } from "recoil";
+import { authState } from "../../contexts/state";
 
 const Home: React.FC = () => {
+  //recoil 사용 선언
+  const auth = useRecoilValue(authState);
+
   return (
     <div>
+      <div className="w-full p-5 bg-blue-400 rounded-br-full">
+        {auth.isLoggedIn
+          ? `userId: ${auth.userId} 님 안녕하세요`
+          : "로그인하고 훈수를 받아보세요!"}
+      </div>
       <HotPost />
     </div>
   );
