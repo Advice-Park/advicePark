871c4fbb3de3d67daeade4ca75cfe56d74a10784
dy <gkekdus97@naver.com>
2024-03-18 21:33:51 +0900

    feat: 유저아이디 추가 및 옵셔널 체이닝 추가

--- src/contexts/state.ts
+++ src/contexts/state.ts
@@ -1,7 +1,8 @@
 import { atom } from "recoil";
 
 export interface IAuthTypes {
-    isLoggedIn: boolean;
+    isLoggedIn?: boolean;
+    userId?: number;
 }
 
 // 로그인 상태
@@ -9,5 +10,6 @@ export const authState = atom<IAuthTypes>({
     key: 'auth',
     default: {
         isLoggedIn: false,
+        userId: 0
     }
 });
