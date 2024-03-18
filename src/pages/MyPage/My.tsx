2ace8eaed915c6cbcc4a2d36148c5cefe0b465e9
dy <gkekdus97@naver.com>
2024-03-18 23:00:18 +0900

    feat: 마이페이지

--- src/pages/MyPage/My.tsx
+++ src/pages/MyPage/My.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Posts, getMyPosts } from "../../services/api/posts";
+import { useNavigate } from "react-router-dom";
 
 const My: React.FC = () => {
+  const navi = useNavigate();
+
+  const [myPosts, setMyPosts] = useState<Posts[]>([]);
+
+  useEffect(() => {
+    getMyPosts().then((res) => {
+      res ? setMyPosts(res) : console.log("내가 쓴 글이 없습니다");
+    });
+  }, []);
+
   return (
     <div>
       <div>My 페이지</div>
+      <div>
+      {myPosts.map((post) => (
+        <ul className="p-5" key={post.postId} onClick={()=>navi(`/posts/${post.postId}`)}>
+          <li>제목: {post.title}</li>
+          <li>{post.contents}</li>
+          <li>{post.category}</li>
+          <li>{post.imageUrls}</li>
+          <li>{post.voteOption}</li>
+          <li>뷰: {post.viewCount}</li>
+          <li>댓글: {post.commentCount}</li>
+        </ul>
+      ))}
+    </div>
     </div>
   );
 };
--- src/services/api/posts.ts
+++ src/services/api/posts.ts
@@ -1,24 +1,34 @@
-import { instance } from "../instance"
+import { instance } from "../instance";
 
 export interface Posts {
-    postId: number
-    userId: number
-    title: string
-    contents: string
-    category: "DAILY" | "LOVE" | "EXERCISE" | "FOOD" | "ETC"
-    viewCount: number
-    commentCount: number
-    createdTime: string
-    imageUrls: string[]
-    voteOption: "YES_NO" | "NORMAL"
+  postId: number;
+  userId: number;
+  title: string;
+  contents: string;
+  category: "DAILY" | "LOVE" | "EXERCISE" | "FOOD" | "ETC";
+  viewCount: number;
+  commentCount: number;
+  createdTime: string;
+  imageUrls: string[];
+  voteOption: "YES_NO" | "NORMAL";
 }
 
 export const getPosts = async () => {
-    try {
-        const res = await instance.get('/api/post')
-        const posts : Posts[] = res.data.result
-        return posts
-    } catch (err) {
-        console.log('posts 불러오기 에러 :', err)
-    }
-}
+  try {
+    const res = await instance.get("/api/post");
+    const posts: Posts[] = res.data.result;
+    return posts;
+  } catch (err) {
+    console.log("posts 불러오기 에러 :", err);
+  }
+};
+
+export const getMyPosts = async () => {
+  try {
+    const res = await instance.get("/api/mypage/post");
+    const myPosts: Posts[] = res.data.result;
+    return myPosts;
+  } catch (err) {
+    console.log("myPosts 불러오기 에러 :", err);
+  }
+};
