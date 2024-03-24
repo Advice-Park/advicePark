4a68f8b2054f2fb0f36638d41415476fc2ef9413
dy <gkekdus97@naver.com>
2024-03-24 20:29:26 +0900

    design: 전체 화면 레이아웃 위치 조정

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar/Navbar";
 const App: React.FC = () => {
   return (
     <>
-      <div className="w-full flex justify-center h-screen mx-auto bg-slate-400">
+      <div className="w-full flex justify-between h-screen mx-auto bg-slate-400">
         {/* ---큰화면용 이미지--- */}
         <div className="md:flex hidden flex-col justify-center h-screen">
           왼쪽 이미지
@@ -14,7 +14,7 @@ const App: React.FC = () => {
 
         {/* ---웹앱 창--- */}
         <div
-          className="max-w-md w-full h-screen overflow-y-scroll bg-gray-100 mx-5"
+          className="max-w-md w-full h-screen overflow-y-scroll bg-gray-100 border border-gray-300"
           suppressHydrationWarning={true}
         >
           <Navbar />
