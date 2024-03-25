0078123da39685c5ca5c5953da68be29e7062615
dy <gkekdus97@naver.com>
2024-03-25 16:25:34 +0900

    design: 댓글 훈수 푸터 고정스타일

--- src/components/comment/WriteComment.tsx
+++ src/components/comment/WriteComment.tsx
@@ -19,8 +19,17 @@ const WriteComment = ({ postId }: CommentProps) => {
   };
 
   return (
-    <div>
-      <input type="text" onChange={(e) => setComment(e.target.value)} />
+    <div className="max-w-md w-full fixed bottom-0 h-20 p-6 flex justify-between bg-white">
+      <input
+        required
+        autoFocus
+        placeholder="최대 300자 훈수를 남겨보세요"
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        maxLength={300}
+        className="max-w-xs w-full p-3 bg-gray-100 rounded"
+      />
       <button onClick={() => addComment(postId)}>등록</button>
     </div>
   );
