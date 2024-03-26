84215b9a401480804de4813d9806ca0816b61fff
dy <gkekdus97@naver.com>
2024-03-26 13:03:08 +0900

    feat: 훈수댓글 삭제 버튼

--- src/components/comment/CommentList.tsx
+++ src/components/comment/CommentList.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { Comment, getComments } from "../../services/api/comment";
+import { instance } from "../../services/instance";
+import { useRecoilValue } from "recoil";
+import { authState } from "../../contexts/state";
 
 type CommentProps = {
   postId: number;
 };
 
 const CommentList = ({ postId }: CommentProps) => {
+  const auth = useRecoilValue(authState);
+
   const [comments, setComments] = useState<Comment[]>([]);
 
   useEffect(() => {
@@ -14,16 +19,31 @@ const CommentList = ({ postId }: CommentProps) => {
     });
   }, []);
 
+  const deleteComment = async (postId: number, commentId: number) => {
+    try {
+      await instance.delete(`/api/comment/${postId}/${commentId}`);
+    } catch (err) {
+      alert("훈수 삭제 에러");
+    }
+  };
+
   return (
     <div className="pb-24">
       {comments.map((post) => (
-        <ul className="p-5" key={post.postId}>
-          <li>댓쓴이: {post.userId}</li>
-          <li>{post.content}</li>
-          <li>{post.likeCount}</li>
-          <li>{post.createdTime}</li>
-          {/* <li><FormattingTime createdTime={post.createdTime} /></li> */}
-        </ul>
+        <>
+          <ul className="p-5" key={post.postId}>
+            <li>댓쓴이: {post.userId}</li>
+            <li>{post.content}</li>
+            <li>{post.likeCount}</li>
+            <li>{post.createdTime}</li>
+            {/* <li><FormattingTime createdTime={post.createdTime} /></li> */}
+          </ul>
+          {auth.userId === post.userId && (
+            <button onClick={() => deleteComment(postId, post.commentId)}>
+              삭제
+            </button>
+          )}
+        </>
       ))}
     </div>
   );
