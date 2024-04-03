eb10e458178094920a956178febf3b00a4c723dc
dy <gkekdus97@naver.com>
2024-04-03 18:24:41 +0900

    feat: 글목록 카테고리 필터 추가

--- src/pages/Posts/PostsPage.tsx
+++ src/pages/Posts/PostsPage.tsx
@@ -8,21 +8,44 @@ import CommentIcon from "../../assets/icons/comment.svg?react";
 const PostsPage: React.FC = () => {
   const navi = useNavigate();
   const [posts, setPosts] = useState<Posts[]>([]);
+  const [filteredPosts, setFilteredPosts] = useState<Posts[]>([]);
 
   useEffect(() => {
     getPosts().then((res) => {
-      res ? setPosts(res) : console.log("글이 없습니다");
+      if (res) {
+        setPosts(res);
+        setFilteredPosts(res);
+      } else {
+        console.log("글이 없습니다");
+      }
     });
   }, []);
+
+  const categoryHandler = (category: string) => {
+    const newFilteredPosts = posts.filter((post) => post.category === category);
+    setFilteredPosts(newFilteredPosts);
+  };
+
+  const categoryResetHandler = () => {
+    setFilteredPosts(posts);
+  };
 
   return (
     <div>
       <h2 className="text-lg font-bold text-center py-3 border-b bg-white">
         훈수게시판
       </h2>
-      {posts.map((post) => (
+      <ul className="flex gap-6 px-8 py-3 bg-white">
+        <li onClick={categoryResetHandler} className="cursor-pointer">전체</li>
+        <li onClick={() => categoryHandler("DAILY")} className="cursor-pointer">일상(잡담)</li>
+        <li onClick={() => categoryHandler("LOVE")} className="cursor-pointer">연애</li>
+        <li onClick={() => categoryHandler("EXERCISE")} className="cursor-pointer">운동</li>
+        <li onClick={() => categoryHandler("FOOD")} className="cursor-pointer">음식</li>
+        <li onClick={() => categoryHandler("ETC")} className="cursor-pointer">기타</li>
+      </ul>
+      {filteredPosts.map((post) => (
         <ul
-          className="py-5 px-8 border-b"
+          className="py-5 px-8 border-b cursor-pointer"
           key={post.postId}
           onClick={() => navi(`/posts/${post.postId}`)}
         >
