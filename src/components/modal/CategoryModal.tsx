b631a61f10966d0492055d677839b57671ae2df0
dy <gkekdus97@naver.com>
2024-04-21 12:22:26 +0900

    design: 글작성 시 카테고리 모달 수정

--- src/components/modal/CategoryModal.tsx
+++ src/components/modal/CategoryModal.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 const CategoryModal: React.FC<Props> = ({ open, close, parentFunction }) => {
   const modalRef = useRef<HTMLDivElement | null>(null);
-  const [isActive, setIsActive] = useState<number>();
+  const [isActive, setIsActive] = useState<number | undefined>();
 
   const closeModal = () => {
     close();
@@ -24,7 +24,8 @@ const CategoryModal: React.FC<Props> = ({ open, close, parentFunction }) => {
     function handleClickOutside(event: Event) {
       if (
         modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
+        !modalRef.current.contains(event.target as Node) &&
+        open
       ) {
         closeModal();
       }
@@ -40,25 +41,31 @@ const CategoryModal: React.FC<Props> = ({ open, close, parentFunction }) => {
   const [category, setCategory] = useState("카테고리");
   const Categories = ["일상(잡담)", "연애", "운동", "음식", "기타"];
 
-  parentFunction(category);
-
   const categoryHandler = (index: number) => {
     setIsActive(index);
     setCategory(Categories[index]);
+    parentFunction(category);
   };
 
   return open ? (
     <>
-      <div onClick={close} />
-      <div ref={modalRef}>
-        <div>
-          <div>
+      <div
+        onClick={close}
+        className="fixed top-0 w-full h-full bg-black opacity-30"
+      />
+      <div
+        ref={modalRef}
+        className="z-3 fixed bottom-0 w-full h-md py-8 shadow bg-white"
+      >
+        <div className="flex">
+          <div className="flex flex-col w-full h-full">
             {Categories.map((item, idx) => (
               <div
                 onClick={() => categoryHandler(idx)}
                 key={idx}
                 // isFirst={idx === 0}
                 // isClicked={isActive}
+                className="w-full px-5 p-3 cursor-pointer hover:text-bold"
               >
                 {item}
               </div>
