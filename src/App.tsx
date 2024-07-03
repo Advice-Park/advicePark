d2075ff83336843292342507fc3e263783984e8b
dy <gkekdus97@naver.com>
2024-07-03 21:14:15 +0900

    fix: 이미지 상대->절대 경로 설정으로 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
       <div className="w-full flex justify-between h-screen mx-auto bg-slate-400">
         {/* ---큰화면용 이미지--- */}
         <div className="md:flex hidden flex-col justify-end h-screen">
-          <img className="w-1/2" src="img/green-dinosaur.png" alt="mainImg" />
-          <img className="" src="img/beach.png" alt="beach" />
+          <img className="w-1/2" src="/img/green-dinosaur.png" alt="mainImg" />
+          <img src="/img/beach.png" alt="beach" />
         </div>
 
         {/* ---웹앱 창--- */}
